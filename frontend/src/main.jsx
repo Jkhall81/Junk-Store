1b37e5a8115d4fdf84bcc01ea6cb679b01d0b48c
Jkhall81 <jason.kei.hall@gmail.com>
2024-01-01 08:09:37 -0700

    added paypal button, created paypal developer account, setup sandboxed accounts for business and private, added createOrder function that sets the currency type and amount to be paid, added my clientID.  Button works on the sandbox account.  now need to set up reducer to change ispaid to true, and set the paidat date.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 import "./index.css";
 import "./bootstrap.min.css";
 
+const initialOptions = {
+  clientId:
+    "AUOYTSvgY-w86-vA1I7n_5as2LvIQ4if1yLiwgwTBLG_91UpPMluLDMb9pBO7bbx_nXsX123insde-MA",
+  currency: "USD",
+  intent: "capture",
+  "data-sdk-integration-source": "integrationbuilder_sc",
+};
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <PayPalScriptProvider options={initialOptions}>
+      <App />
+    </PayPalScriptProvider>
   </React.StrictMode>
 );
--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -1,24 +1,36 @@
 import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import { Button, Row, Col, ListGroup, Image, Card } from "react-bootstrap";
-import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { Row, Col, ListGroup, Image, Card } from "react-bootstrap";
 import Message from "../components/Message";
 import { Link } from "react-router-dom";
 import { useGetOrderByIdQuery } from "../redux/services/orderApi";
 import { useGetOrderItemByOrderIdQuery } from "../redux/services/orderApi";
+import { PayPalButtons, usePayPalScriptReducer } from "@paypal/react-paypal-js";
 
 const OrderScreen = () => {
-  const { ...cart } = useSelector((state) => state.cart);
-  const { userInfo } = useSelector((state) => state.user);
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
   const { id } = useParams();
   const { data: order } = useGetOrderByIdQuery({ id });
   const { data: orderItem } = useGetOrderItemByOrderIdQuery({ id });
   const [shippingAddress, setShippingAddress] = useState({});
+  const [payPalOrder, setPayPalOrder] = useState(null);
+  // const [{ isPending }] = usePayPalScriptReducer();
   const name = JSON.parse(localStorage.getItem("userInfo"));
-  console.log(order);
-  // console.log(orderItem);
+
+  const createOrder = (data, actions) => {
+    return actions.order.create({
+      purchase_units: [
+        {
+          amount: {
+            currency_code: "USD",
+            value: order?.totalPrice,
+          },
+        },
+      ],
+    });
+  };
+
+  // console.log(order);
+  console.log(orderItem);
   // console.log(shippingAddress);
 
   useEffect(() => {
@@ -28,110 +40,140 @@ const OrderScreen = () => {
     }
   }, [orderItem]);
 
-  const placeOrder = async () => {};
   return (
     <div>
-      <Row>
-        <h1>Order: {id}</h1>
-        <Col md={8}>
-          <ListGroup variant="flush">
-            <ListGroup.Item>
-              <h2>Shipping</h2>
-              <p>
-                Name: {name.first_name} {name.last_name}
-              </p>
-              <p>
-                <strong>Shipping: </strong>
-                {shippingAddress.address}, {shippingAddress.city}{" "}
-                {shippingAddress.postalCode}, {shippingAddress.country}
-              </p>
-            </ListGroup.Item>
-
-            <ListGroup.Item>
-              <h2>Payment Method</h2>
-              <p>
-                <strong>Method: </strong>
-                {order?.paymentMethod}
-              </p>
-            </ListGroup.Item>
-
-            <ListGroup.Item>
-              <h2>Order Items</h2>
-              {orderItem?.order_items.length === 0 ? (
-                <Message variant="info">No items in this order!</Message>
-              ) : (
-                <ListGroup variant="flush">
-                  {orderItem?.order_items.map((item, index) => (
-                    <ListGroup.Item key={index}>
-                      <Row>
-                        <Col md={2}>
-                          <Image
-                            src={item.image}
-                            alt={item.name}
-                            fluid
-                            rounded
-                          />
-                        </Col>
-                        <Col>
-                          <Link to={`/product/${item._id}`}>{item.name}</Link>
-                        </Col>
-
-                        <Col md={4}>
-                          {item.qty} X ${item.price} = $
-                          {(item.qty * item.price).toFixed(2)}
-                        </Col>
-                      </Row>
-                    </ListGroup.Item>
-                  ))}
-                </ListGroup>
-              )}
-            </ListGroup.Item>
-          </ListGroup>
-        </Col>
-
-        <Col md={4}>
-          <Card>
+      {orderItem ? (
+        <Row>
+          <h1>Order: {id}</h1>
+          <Col md={8}>
             <ListGroup variant="flush">
               <ListGroup.Item>
-                <h2>Order Summary</h2>
+                <h2>Shipping</h2>
+                <p>
+                  Name: {name.first_name} {name.last_name}
+                </p>
+                <p>
+                  Email:{" "}
+                  <strong>
+                    <a href={`mailto:${name.username}`}>{name.username}</a>
+                  </strong>
+                </p>
+                <p>
+                  <strong>Shipping: </strong>
+                  {shippingAddress.address}, {shippingAddress.city}{" "}
+                  {shippingAddress.postalCode}, {shippingAddress.country}
+                </p>
+
+                {order?.isDelivered ? (
+                  <Message variant="success">
+                    Delivered on {order.deliveredAt}
+                  </Message>
+                ) : (
+                  <Message variant="warning">Not Delivered</Message>
+                )}
               </ListGroup.Item>
 
               <ListGroup.Item>
-                <Row>
-                  <Col>Items:</Col>
-                  <Col>
-                    $
-                    {orderItem?.order_items
-                      .reduce((acc, x) => acc + Number(x.price), 0)
-                      .toFixed(2)}
-                  </Col>
-                </Row>
+                <h2>Payment Method</h2>
+                <p>
+                  <strong>Method: </strong>
+                  {order?.paymentMethod}
+                </p>
+                {order?.isPaid ? (
+                  <Message variant="success">Paid on {order?.paidAt}</Message>
+                ) : (
+                  <Message variant="warning">Not paid</Message>
+                )}
               </ListGroup.Item>
 
               <ListGroup.Item>
-                <Row>
-                  <Col>Shipping:</Col>
-                  <Col>${order?.shippingPrice}</Col>
-                </Row>
-              </ListGroup.Item>
+                <h2>Order Items</h2>
+                {orderItem?.order_items.length === 0 ? (
+                  <Message variant="info">No items in this order!</Message>
+                ) : (
+                  <ListGroup variant="flush">
+                    {orderItem?.order_items.map((item, index) => (
+                      <ListGroup.Item key={index}>
+                        <Row>
+                          <Col md={2}>
+                            <Image
+                              src={item.image}
+                              alt={item.name}
+                              fluid
+                              rounded
+                            />
+                          </Col>
+                          <Col>
+                            <Link to={`/product/${item._id}`}>{item.name}</Link>
+                          </Col>
 
-              <ListGroup.Item>
-                <Row>
-                  <Col>Tax:</Col>
-                  <Col>${order?.taxPrice}</Col>
-                </Row>
-              </ListGroup.Item>
-
-              <ListGroup.Item>
-                <Row>
-                  <Col>Total:</Col>
-                  <Col>${order?.totalPrice}</Col>
-                </Row>
+                          <Col md={4}>
+                            {item.qty} X ${item.price} = $
+                            {(item.qty * item.price).toFixed(2)}
+                          </Col>
+                        </Row>
+                      </ListGroup.Item>
+                    ))}
+                  </ListGroup>
+                )}
               </ListGroup.Item>
             </ListGroup>
-          </Card>
-        </Col>
-      </Row>
+          </Col>
+
+          <Col md={4}>
+            <Card>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <h2>Order Summary</h2>
+                </ListGroup.Item>
+
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Items:</Col>
+                    <Col>
+                      $
+                      {orderItem?.order_items
+                        .reduce((acc, x) => acc + Number(x.price), 0)
+                        .toFixed(2)}
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Shipping:</Col>
+                    <Col>${order?.shippingPrice}</Col>
+                  </Row>
+                </ListGroup.Item>
+
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Tax:</Col>
+                    <Col>${order?.taxPrice}</Col>
+                  </Row>
+                </ListGroup.Item>
+
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Total:</Col>
+                    <Col>${order?.totalPrice}</Col>
+                  </Row>
+                </ListGroup.Item>
+              </ListGroup>
+              {!order?.isPaid && (
+                <ListGroup.Item>
+                  <PayPalButtons
+                    style={{ layout: "vertical" }}
+                    createOrder={createOrder}
+                  />
+                </ListGroup.Item>
+              )}
+            </Card>
+          </Col>
+        </Row>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 };
