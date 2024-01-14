caf681952737f51a3680c026fe83b44367c84331
Jkhall81 <jason.kei.hall@gmail.com>
2024-01-14 13:16:32 -0700

    test4.;

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -187,7 +187,8 @@ STATICFILES_DIRS = [
 ]
 
 # for user uploaded content!
-MEDIA_ROOT = 'static/images'
+MEDIA_URL = '/static/images'
+MEDIA_ROOT = BASE_DIR / 'static/images'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
