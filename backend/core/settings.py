68590fe659e953d6fea967d36bcfd546c0d24f46
Jkhall81 <jason.kei.hall@gmail.com>
2024-01-14 12:43:17 -0700

    still having static file issue.  something isnt configured right.

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -20,7 +20,7 @@ env = environ.Env(DEBUG=(bool, False))
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 environ.Env.read_env(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
@@ -180,8 +180,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = '/images/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
@@ -190,7 +189,7 @@ STATICFILES_DIRS = [
 # for user uploaded content!
 MEDIA_ROOT = 'static/images'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
